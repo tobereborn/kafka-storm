093b97cc6a18ab21eb43311e9c6cfea5b4297a31
tobereborn <jinweizhen008@gmail.com>
2016-05-11 10:33:05 +0800

    Project initialization

---
+++ src/main/java/com/hibigdata/kafka_storm/App.java
@@ -0,0 +1,13 @@
+package com.hibigdata.kafka_storm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
